50f9387b8218990c7e5e05c51da029562bb497dc
zhengguang <zg.zhu@daocloud.io>
2016-07-07 12:17:58 +0800

    support config file and remove orphan containers

---
+++ parse_config_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import yaml
+import sys
+
+if len(sys.argv) < 3:
+  sys.exit(1)
+
+for yml in sys.argv[2:]:
+  for svc, content in yaml.load(open(yml, "r")).items():
+    prefix = "constraint:node=="
+    node = ""
+    for env in content["environment"]:
+      if env.startswith(prefix):
+        node = env[len(prefix):]
+        break
+    for vol in content["volumes"]:
+      if sys.argv[1]+":" in vol:
+        print svc, node, vol.split(":")[0]
